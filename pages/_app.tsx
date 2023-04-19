87946ce1d5656f46ea6c684c0a4ae06999dbc7ef
0xKarasu <0xKarasu@protonmail.com>
2023-04-19 19:12:13 +0200

    :green_heart: fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import Head from 'next/head'
-import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Main from '../components/main'
-import Script from 'next/script'
 const Home: NextPage = () => {
     return (
         <div className="flex max-h-screen flex-col items-center justify-center py-2">
